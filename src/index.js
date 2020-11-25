2ac330f24992c71f24da690a0afb94e276812b0a
platon <platon.s14@list.ru>
2020-11-25 17:14:04 +0500

    adding list of cards and robots enumeration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import { robots } from './robots';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <CardList robots={robots} />
     </div>
   </React.StrictMode>,
   document.getElementById('root')
