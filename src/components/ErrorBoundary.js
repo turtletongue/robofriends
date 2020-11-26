eafb7b3fc124263d46de9c3f5ebf1847390d6a94
platon <platon.s14@list.ru>
2020-11-26 19:14:56 +0500

    Добавление обработчика ошибок и небольшие исправления в синтаксисе.

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+    
+    componentDidCatch(error, info) {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                    <h1>Oooops, this isn't good.</h1>
+                );
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
